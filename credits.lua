075d202c5fff9017c5021aacb258f9df515faacb
Chris Saunders <chris.saunders@shopify.com>
2015-01-25 17:20:07 +0000

    Cleaned up and added audio files
    Updated credits file with effect author details

--- credits.lua
+++ credits.lua
@@ -1,6 +1,23 @@
-music = {
-  "Water Lily",
-  "by Kevin MacLeod (incompetech.com)",
-  "Licensed under Creative Commons: By Attribution 3.0",
-  "http://creativecommons.org/licenses/by/3.0/"
+Credits = {}
+
+Credits['Music'] = {
+  {
+    "Water Lily",
+    "by Kevin MacLeod (incompetech.com)",
+    "Licensed under Creative Commons: By Attribution 3.0",
+    "http://creativecommons.org/licenses/by/3.0/"
+  }
+}
+
+Credits['Effects'] = {
+  {
+    "Lighting Torch Sound",
+    "Krisboruf",
+    "Licensed under Creative Commons: By Attribution 3.0",
+    "http://creativecommons.org/licenses/by/3.0/"
+  }
+}
+
+Credits['Art'] = {
+
 }
